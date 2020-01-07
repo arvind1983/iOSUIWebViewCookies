0b2c98c1c05f0be8c09c7ec10ca1f9007949d6a8
arvind1983 <41881552+arvind1983@users.noreply.github.com>
2020-01-07 13:04:26 +0800

    Update UIWebViewCookies.m

--- src/ios/UIWebViewCookies.m
+++ src/ios/UIWebViewCookies.m
@@ -129,51 +129,7 @@
 -(void)deleteAllCookies:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
-    
-    /*NSHTTPCookieStorage *storage = [NSHTTPCookieStorage sharedHTTPCookieStorage];
-    for (NSHTTPCookie *cookie in [storage cookies]) {
-        [storage deleteCookie:cookie];
-    }*/
-    
-    /*
-    //Additional not tested
-    NSURLCache *sharedCache = [[NSURLCache alloc] initWithMemoryCapacity:0 diskCapacity:0 diskPath:nil];
-    [NSURLCache setSharedURLCache:sharedCache];
-
-    //Clear All Cookies
-    for(NSHTTPCookie *cookie in [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookies]) {
-        [[NSHTTPCookieStorage sharedHTTPCookieStorage] deleteCookie:cookie];
-    }
-    
-    //Remove the localstorage db
-    NSString *path = [[[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent:@"Backups"] stringByAppendingPathComponent:@"localstorage.appdata.db"];
-    [[NSFileManager defaultManager] removeItemAtPath:path error:nil];
-
-    //Also remove the cached versions
-    path = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject];
-    for (NSString *string in [[NSFileManager defaultManager] contentsOfDirectoryAtPath:path error:nil]) {
-        if ([[string pathExtension] isEqualToString:@"localstorage"]) {
-            [[NSFileManager defaultManager] removeItemAtPath:path error:nil];
-        }
-    }
-    
-    [[NSURLCache sharedURLCache] removeAllCachedResponses]; // remove all cache responses
         
-    UIAlertView *toast = [
-            [UIAlertView alloc] initWithTitle:@"Deleted Cookies"
-            message:@"Deleting cookies!"
-            delegate:nil
-            cancelButtonTitle:nil
-            otherButtonTitles:nil, nil];
-
-        [toast show];
-        
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-            [toast dismissWithClickedButtonIndex:0 animated:YES];
-        });
-    
-    [[NSUserDefaults standardUserDefaults] synchronize];*/
-    
     // alert stuffs
     NSInteger *cookieCount = 0;
     NSMutableString * aTitleString = [NSMutableString stringWithFormat:@"Deleted %d", cookieCount];    // does not need to be released. Needs to be retained if you need to keep use it after the current function.
@@ -221,7 +177,7 @@
             cancelButtonTitle:nil
             otherButtonTitles:nil, nil];
 
-        [toast show];
+        //[toast show];
         
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
             [toast dismissWithClickedButtonIndex:0 animated:YES];
