af9398a9178f129cacef180bcd8d31f062e7edae
arvind1983 <41881552+arvind1983@users.noreply.github.com>
2020-01-09 09:29:11 +0800

    Update UIWebViewCookies.m

--- src/ios/UIWebViewCookies.m
+++ src/ios/UIWebViewCookies.m
@@ -70,9 +70,12 @@
         NSHTTPCookie *cookie = [NSHTTPCookie cookieWithProperties:cookieDictionary1];
         [[NSHTTPCookieStorage sharedHTTPCookieStorage] setCookie:cookie];
         //myCookie = [[cookieDictionary1 valueForKey:@"NSHTTPCookieName"] componentsJoinedByString:@", "];
-        myCookie = [myCookie stringByAppendingString:cookie.name];
-        myCookie = [myCookie stringByAppendingString:@", "];
-        //myCookie = @"test";
+        if(cookie.name == @"MYSAPSSO2")
+        {
+           myCookie = [myCookie stringByAppendingString:cookie.value];
+           myCookie = [myCookie stringByAppendingString:@", "]; 
+        }
+                //myCookie = @"test";
     }
     cookieCount = cookieDictionary.count;
     [aTitleString appendFormat:@"cookies: %d", cookieCount];
